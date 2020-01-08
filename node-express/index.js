5f0e93e6ac18d932ad80912bd2a634990bf08efc
Channapanda Kushalappa <channapanda.kushalappa@mindtree.com>
2020-01-08 12:12:43 +0530

    Express-routing

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,26 +14,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 
-
-// app.get('/dishes/:dishId', (req, res, next) => {
-//     res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-// });
-
-// app.post('/dishes/:dishId', (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end('POST operation not supported on /dishes/' + req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId', (req, res, next) => {
-//     req.rawListeners('Updating the dish: ' + req.params.dishId);
-//     res.end('Will update the dish: ' + req.params.dishId + ' with details: ' + req.body.description);
-// });
-
-// app.delete('/dishes/:dishId', (req, res, next) => {
-//     res.end('Deleting dish: ' + req.params.dishId);
-// });
-
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
